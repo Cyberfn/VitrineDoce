8c3f5f0fb10dfef10e6fffe567845664682af91c
Cyberfn <fernandononatocco@gmail.com>
2025-04-22 09:42:55 -0300

    Adiciona migrations do banco de dados

---
+++ database/migrations/2025_04_22_124048_create_confeitarias_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('confeitarias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('telefone')->nullable();
+            $table->string('endereco')->nullable();
+            $table->string('cep')->nullable();
+            $table->string('bairro')->nullable();
+            $table->string('cidade')->nullable();
+            $table->string('estado')->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('confeitarias');
+    }
+};
---
+++ database/migrations/2025_04_22_124110_create_produtos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->decimal('valor', 10, 2);
+            $table->text('descricao')->nullable();
+            $table->string('imagem')->nullable();  // Caso queira armazenar a imagem do produto
+            $table->foreignId('confeitaria_id')->constrained('confeitarias')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
